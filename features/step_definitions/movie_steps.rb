c351041fbf3eccd0d8384396538a46ba86d6f4d1
slychika <heather.dykstra@colorado.edu>
2013-11-06 18:34:02 -0800

    All working

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -2,10 +2,10 @@
 
 Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
+    Movie.create!(movie)
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
   end
-  flunk "Unimplemented"
 end
 
 # Make sure that one string (regexp) occurs before or after another one
@@ -14,7 +14,12 @@ end
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
   #  ensure that that e1 occurs before e2.
   #  page.body is the entire content of the page as a string.
-  flunk "Unimplemented"
+  #  flunk "Unimplemented"
+  #  x = page.body.split(e1)
+  #  assert x.length == 3
+  #  x = x[2].split(e2)
+  #  assert x.length == 3
+  page.body.should =~ /#{e1}.*#{e2}/m
 end
 
 # Make it easier to express checking or unchecking several boxes at once
@@ -25,4 +30,29 @@ When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
+  rating_list.split(%r{\s*,\s*}).each_with_index {
+    |name, index|
+      if(!index)
+        When %Q{I #{uncheck}check "ratings[#{name}]"}
+      else
+        And %Q{I #{uncheck}check "ratings[#{name}]"}
+      end
+  }
+  #rating_list.split(/, /).each do |rating|
+  #  rating = "ratings_#{rating}"
+  #  uncheck ? uncheck(rating) : check(rating)
+  #end
+end
+
+Then /I should( not)? see all of the movies/ do |orNot|
+  Movie.all.each_with_index {
+    |movie, index|
+      name = movie[:title]
+      if(!index)
+        When %Q{I should#{orNot} see "#{name}"}
+      else
+        And %Q{I should#{orNot} see "#{name}"}
+      end
+  }
+  
 end
